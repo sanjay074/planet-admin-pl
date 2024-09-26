5051b970b3827f1648d96397eac99fbf5e9b5aea
Ranjay Kumar <53505123+ranjaykumar73@users.noreply.github.com>
2024-09-26 18:11:49 +0530

    dddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './Components/Layout/Sidebar';
+import Sidebar from './Components/Layouts/Sidebar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
