d5ad0b7a4cc59764c3a4dcc55cf4a3c1ce72207a
Ranjay Kumar <53505123+ranjaykumar73@users.noreply.github.com>
2024-09-26 15:32:35 +0530

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Sidebar from './Components/Layout/Sidebar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <div className='bg-red-500'>
-      helllo
+    <Router>
+    <Routes>
+      <Route/>
+      <Route
+          path="*"
+          element={
+            <div className="flex flex-col h-screen">
+
+           <Sidebar/>
+            </div>
+          }
+          />
+    </Routes>
+    </Router>
     </div>
   ) 
 }
---
+++ src/Components/Icons/RightArrowIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightArrowIcon = () => {
+  return (
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+    </div>
+  )
+}
+
+export default RightArrowIcon
