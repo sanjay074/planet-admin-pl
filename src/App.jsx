ccf9aa69ff4c859a3ea7d63d045ccf87cf66fe72
Ranjay Kumar <53505123+ranjaykumar73@users.noreply.github.com>
2024-09-27 11:13:42 +0530

    changes in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import Sidebar from './Components/Layouts/Sidebar';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './Components/Layouts/Sidebar';
+// import Header from './components/Header';
+import Dashboard from './components/dashboard/Dashboard';
+import { Outlet } from 'react-router-dom';
+import Login from './pages/Login';
+import Header from './Components/Header';
+
+const Layout = () => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-64">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col w-full h-full">
+        <Header />
+        <div className="flex-1 overflow-y-auto p-4">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-    <Routes>
-      <Route/>
-      <Route
-          path="*"
-          element={
-            <div className="flex flex-col h-screen">
-
-           <Sidebar/>
-            </div>
-          }
-          />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </Router>
   );
 };
